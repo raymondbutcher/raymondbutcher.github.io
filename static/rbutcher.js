95083e27a45cbf4f8b4a5e38ab68ee2d5a7989cb
Raymond Butcher <randomy@gmail.com>
2015-04-08 17:17:34 +0100

    Improved the lazy loading of photos.
    The transparent gifs aren’t great but I chose to work within the
    limitations of the gallery plugin for now.

--- static/rbutcher.js
+++ static/rbutcher.js
@@ -1,15 +1,12 @@
 /*
 The image tags are being placed inside a noscript tag, making
-this page work with crawlers and browsers with JavaScript disabled.
+this page work with crawlers and browsers without JavaScript support.
 
-For browsers with JavaScript enabled, the noscript tag prevents
-the images from being downloaded, at least until they're added
-to the DOM via JavaScript.
-
-This script will append the images to the DOM lazily when the user
-scrolls down. This avoids downloading megabytes of images until
-required, while still degrading nicely when there is no JavaScript
-support.
+For browsers with JavaScript support, the noscript tag prevents the images
+from being rendered and downloaded. This script moves the images into the DOM,
+but changes the image URL to a tiny transparent image. When the user scrolls
+down the page, the image URLs are swapped to the real ones, and the images
+are displayed as normal.
 */
 
 // Remove and return an element from a jQuery object.
@@ -25,38 +22,37 @@ $.fn.popRandom = function() {
     );
 }
 
+// Fetch the DOM elements from a <noscript> tag, which is treated as raw text
+// by the browser. Optionally, provide a transform function for the raw text,
+// allowing it to be changed before being turned into DOM elements.
+$.fn.noscript = function(transform) {
+    var html = this.find('noscript').text();
+    if (transform) {
+        html = transform(html);
+    }
+    return $('<div>' + html + '</div>').children();
+}
+
 // Run when the document is ready.
 $(function() {
+    var $gallery = $('#gallery');
 
-    var $photos = $('#photos');
+    // Move the <noscript> photos into the DOM.
+    var $photos = $gallery.noscript(function(html) {
+        // Enable lazy loading.
+        return html.replace(/ src=/g, ' src="./static/transparent.gif" class="lazy" data-src=');
+    });
+    while ($photos.length) {
+        $gallery.append($photos.popRandom());
+    }
 
-    // Extract the noscript text (it is text, not DOM elements).
-    var html = $photos.find('noscript').text();
+    var rowHeight = 300;
 
-    // Swap out the src attributes to avoid downloading the images.
-    var htmlNoDownload = html.replace(/ src=/g, ' data-src=');
-
-    // Create a queue of DOM elements, each a photo.
-    var $queue = $('<div>' + htmlNoDownload + '</div>').children();
-
-    // Find the smallest height value from all of the photos
-    // and use that as the minimum height in the gallery.
-    var minHeight = (function() {
-        var min = null;
-        $queue.find('img').each(function() {
-            var height = parseInt($(this).attr('height'));
-            if (min == null || height < min) {
-                min = height;
-            }
-        });
-        return min;
-    })();
-
-    // Initialize the gallery, even though there are no photos yet.
-    $photos.justifiedGallery({
+    // Initialize the gallery, even though there are no photos in it yet.
+    $gallery.justifiedGallery({
         lastRow: 'hide',
         margins: 0,
-        rowHeight: minHeight,
+        rowHeight: 300,
         sizeRangeSuffixes: {
             'lt100':'_t',
             'lt240':'_m',
@@ -68,37 +64,34 @@ $(function() {
         waitThumbnailsLoad: false
     });
 
-    // This adds one random photo from the queue.
-    // Returns true if successful, else false.
-    function addPhoto() {
-        var $photo = $queue.popRandom();
+    var $window = $(window);
 
-        if ($photo.length == 0) {
-            return false;
-        }
+    // Add photos to the gallery when scrolled near to the bottom of the page.
+    $window.on('scroll.rb', function(){
+        var loadLine = $window.scrollTop() + $window.height() + rowHeight/2,
+            loaded = false;
+        while (true) {
+            var $img = $gallery.find('img.lazy').first();
 
-        // Restore the src attribute so it downloads and displays.
-        var $img = $photo.find('img');
-        $img.attr('src', $img.data('src'));
-
-        $photos.append($photo);
-
-        return true;
-    }
-
-    // Add photos when scrolled near to the bottom of the window.
-    $(window).on('scroll.rb', function(){
-        while ($(window).height() + $(window).scrollTop()
-                >= $(document).height() - minHeight*1.5) {
-            // Add a photo.
-            if (addPhoto()) {
-                // It returned true, there are more photos left. so update the gallery positions.
-                $photos.justifiedGallery('norewind');
-            } else {
-                // Out of photos, so stop it.
+            if ($img.length == 0) {
+                // All images have been loaded.
                 $(window).off('scroll.rb');
                 break;
             }
+
+            if ($img.offset().top > loadLine) {
+                // Still hidden, stop checking.
+                break;
+            }
+
+            // Load the real image.
+            $img.attr('src', $img.data('src'));
+            $img.data('jg.loaded', false);
+            $img.removeClass('lazy');
+            loaded = true;
+        }
+        if (loaded) {
+            $gallery.justifiedGallery();
         }
     }).scroll();
 });
